9d70c84b098bef88236734320d5d39196da0a390
Jack Dewinter <jackdewinter1@gmail.com>
2018-04-06 10:06:54 +0100

    feature test for filling names in.

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -7,9 +7,10 @@ feature 'Home Page' do
 		visit '/'
 		# @!method input_name Name of the input tag.
 		# @!method input_fill The text to place in the tag.
-		fill_in 'Player1', with: 'ash-ketchum'
-		fill_in 'Player2', with: 'may'
-		expect()
+		fill_in 'Player1', with: 'Ash Ketchum'
+		fill_in 'Player2', with: 'May'
+		click_button('Submit')
+		expect(page).to have_content('Ash Ketchum VS My')
 	end
 end
 
