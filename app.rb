e9d12baf03c83298863cf49c94bc871bc6912f15
Jack Dewinter <jackdewinter1@gmail.com>
2018-04-06 11:23:16 +0100

    testing sessions

--- app.rb
+++ app.rb
@@ -2,14 +2,20 @@ require 'sinatra/base'
 
 # Battle class
 class Battle < Sinatra::Base
+	enable :sessions
 	get '/' do
-		erb(:index)
+		erb :index
 	end
 
 	post '/names' do
 		@player1 = params[:player1]
 		@player2 = params[:player2]
+		session['p1test'] = params[:player1]
 		erb :play
+	end
+
+	get '/sesh' do
+		"p1 = " << session[:p1test].inspect
 	end
 
 	run! if app_file == $0
